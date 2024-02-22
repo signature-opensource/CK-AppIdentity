0bfb9b10e0723d851484c2fa3b0f3993bf3ab8a4
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-02-22 16:09:50 +0100

    Removed TryAdjustCurrentTime.
    If it happens to be useful and doable, we'll restore it later.

--- CK.AppIdentity/ApplicationIdentityService.ISystemClock.cs
+++ CK.AppIdentity/ApplicationIdentityService.ISystemClock.cs
@@ -6,23 +6,14 @@ namespace CK.AppIdentity
     public sealed partial class ApplicationIdentityService
     {
         /// <summary>
-        /// Optional system clock that when available in the DI container provided  enables the time to be set and
-        /// the heart beat rate used by the back tasks to be configured.
+        /// Optional system clock that when available in the DI container enables to configure
+        /// the <see cref="ApplicationIdentityService.Heartbeat"/> rate.
         /// <para>
         /// This should be used mainly for tests. When not registered in tne DI container, <see cref="DefaultClock"/> is used.
         /// </para>
         /// </summary>
         public interface ISystemClock : Core.ISystemClock
         {
-            /// <summary>
-            /// Attempts to set the ambient system (or process) time by adding <paramref name="offset"/> to the
-            /// current <see cref="Core.ISystemClock.UtcNow"/> value.
-            /// </summary>
-            /// <param name="monitor">The monitor to use.</param>
-            /// <param name="offset">The offset to apply.</param>
-            /// <returns>True on success, false if adjusting time failed or is not possible.</returns>
-            bool TryAdjustCurrentTime( IActivityMonitor monitor, TimeSpan offset );
-
             /// <summary>
             /// Gets the heart beat period in milliseconds.
             /// Defaults to 1000 ms (1 second that is the maximum).
@@ -36,12 +27,6 @@ namespace CK.AppIdentity
             public DateTime UtcNow => DateTime.UtcNow;
 
             public int HeatBeatPeriod => 1000;
-
-            public bool TryAdjustCurrentTime( IActivityMonitor monitor, TimeSpan offset )
-            {
-                monitor.Error( "TryAdjustCurrentTime is not implemented yet." );
-                return false;
-            }
         }
 
         /// <summary>
